4ddc3393baf12f89383216767908f82ca2cd469d
Pasquale Cantiello <pasquale.cantiello@unicampania.it>
2018-04-07 10:33:27 +0200

    Commit iniziale

---
+++ app/src/main/java/it/unicampania/uniapp/ListaStudentiActivity.java
@@ -0,0 +1,13 @@
+package it.unicampania.uniapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaStudentiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_studenti);
+    }
+}
